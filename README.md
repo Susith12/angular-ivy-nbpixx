db69f2d84741c4d4c430f5c77c8d23ce31579ef6
Susith12 <iamsusithalwis@gmail.com>
2022-05-18 14:09:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-nbpixx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-nbpixx)
---
+++ src/app/Services/supplier.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SupplierResponse } from '../Models/supplier-response.model';
+import { Supplier } from '../Models/supplier.model';
+import { Observable } from 'rxjs';
+import { AddSupplierResponse } from '../Models/add-supplier-respponse.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupplierService {
+  private baseUrl = 'https://host1.open.uom.lk/';
+  constructor(private http : HttpClient) { }
+
+  getSuppliers(): Observable<SupplierResponse> {
+    return this.http.get<SupplierResponse>(this.baseUrl + 'api/suppliers/');
+  }
+}
